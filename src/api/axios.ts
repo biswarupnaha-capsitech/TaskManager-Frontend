4741a38724f9e7ce65b3005512ca5853873f8c02
biswarupnaha-capsitech <biswarup.naha@capsitech.com>
2026-08-07 18:33:03 +0530

    update axios config

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -10,7 +10,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem("tm-accessToken");
+    const token = localStorage.getItem("tm-access");
     if (token) config.headers["Authorization"] = `Bearer ${token}`;
     config.withCredentials = true;
     return config;
